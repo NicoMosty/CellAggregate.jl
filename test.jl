ec53cde16751972a602aa565d453d644ba44eee1
NicoMosty <40640296+NicoMosty@users.noreply.github.com>
2022-07-18 01:33:51 -0500

    Adding Delauney From Scypy

--- test.jl
+++ test.jl
@@ -1,12 +1,13 @@
 using CSV: read, write
 using DataFrames
 using Delaunay: delaunay
-using GMT: triangulate
 
-for R_Agg in 2:1:2
+for R_Agg in 2:1:4
     println("_____ R_Agg= $R_Agg _____")
     @time df = read("data/Init/Two_Sphere/$R_Agg.csv", DataFrame)
     @time df_temp = Matrix(df[!, [:X, :Y, :Z]])
-    @time A = triangulate(df_temp)
-    println(map(x->x[1],A))
+    @time global A = delaunay(df_temp).vertex_neighbor_vertices
+    # @time global A = delaunay(df_temp)
+    println("_____")
+    println(A.1)
 end
