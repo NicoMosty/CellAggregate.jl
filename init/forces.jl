4cd96eb90a71b244daca49000a48ceb6daf68e10
NicoMosty <npuentesu@unal.edu.co>
2022-09-26 17:25:12 -0500

    add: adding changes in fusion of aggregates

--- init/forces.jl
+++ init/forces.jl
@@ -1,5 +1,6 @@
 using LinearAlgebra: norm
-using NearestNeighbors
+# using NearestNeighbors
+using CUDA
 
 function force(X,kdtree,  r_max, s, K )
     # Initialise displacement array
@@ -26,4 +27,32 @@ function force(X,kdtree,  r_max, s, K )
         end
     end
     return dX
+end
+
+function forces_cu()
+    # Definig Variables for calculing dX
+    global X; global dX; global idx
+
+    # Finding Distances
+    r = reshape(repeat(X, inner=(nn,1)), nn, size(X)[1], 3) - X[getindex.(idx,1),:]
+
+    # Finding Distances/Norm
+    dist = (r[:,:,1] .^ 2 + r[:,:,2] .^ 2 + r[:,:,3] .^ 2) .^ (0.5)
+    dist = reshape(repeat((dist), outer=(1,3)) ,nn ,size(X)[1], 3)
+    
+    # Normalizationg Distances
+    r = r ./ dist
+
+    # dX[i,:] +=  r/dist * F
+    F = -K.*((dist .- r_max).^2) .* (dist .- s) .* r
+
+    # Deleting Forces greater than R_Max
+    F[dist .>  r_max] .= 0
+
+    # Calculating de dX
+    dX[:,1] = sum(F[:,:,1][2:end,:]; dims=1)
+    dX[:,2] = sum(F[:,:,2][2:end,:]; dims=1)
+    dX[:,3] = sum(F[:,:,3][2:end,:]; dims=1)
+
+    synchronize()
 end
