8357fa4728a9b3d1bf4a80c3821509672f59ac8d
NicoMosty <npuentesu@unal.edu.co>
2022-10-15 16:08:38 -0500

    add: adding CPU testing

--- src/forces.jl
+++ src/forces.jl
@@ -2,27 +2,22 @@ using LinearAlgebra: norm
 # using NearestNeighbors
 using CUDA
 
-function force(X,kdtree,  r_max, s, K )
+function force(X, idxs, r_max, s, K )
     # Initialise displacement array
     global dX = zeros(Float64, size(X)[1], 3)
 
-    # Loop over all cells to compute displacements
     for i in 1:size(X)[1]
-        # Scan neighbours
-        global idxs, _ = knn(kdtree, X[i,1:3], 14, true)
-
         # Initialise variables
         global Xi = X[i,1:3]
-        for j in idxs
-        # for j in X[j,1:3]
+        for j in idxs[:,i]
             if i != j
-                global r = Xi - X[j,1:3]
+                global r = Xi - X[j,:]
                 global dist = norm(r)
                 # Calculate attraction/repulsion force differential here
                 if dist < r_max
                     global F = - K*(dist-r_max)*(dist-r_max)*(dist - s)
-                    dX[i,:] +=  r/dist * F
-                end
+                    dX[i,:] =  dX[i,:] + r/dist * F
+                end 
             end
         end
     end
