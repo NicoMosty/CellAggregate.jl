077d40b8e3a901f5114c2a9e5d07b05b402fbb31
NicoMosty <npuentesu@unal.edu.co>
2022-10-21 22:22:44 -0500

    add: adding kn  function to new path

--- src/Neighbor.jl
+++ src/Neighbor.jl
@@ -1,8 +1,19 @@
 using CUDA
+using NearestNeighbors
 
-function knn_cu()
+function cpu_knn(X, nn)
+    kdtree = KDTree(X')
+    idx = Int.(zeros(nn,size(X,1)))
+    for i in 1:size(X)[1]
+        # Scan neighbours
+        idx[:,i], _ = knn(kdtree, X[i,1:3], nn, true)
+    end
+    return Matrix(idx)
+end
+
+function cu_knn()
     # Definig Variables for calculing knn
-    global i_Cell; global Dist; global idx
+    global i_Cell; global Dist; global idx; global X
     
     # Defining Coordinates of each cell on the aggregates
     i_Cell = reshape(repeat(X, size(X ,1)), size(X ,1), size(X ,1), 3) - reshape(repeat(X, inner=(size(X ,1),1)), size(X ,1), size(X ,1), 3)
@@ -16,7 +27,5 @@ function knn_cu()
         idx[i,:] = findmin(Dist; dims=1)[2]
         Dist[idx[i,:]] .= Inf
     end
-    # Dist = nothing; GC.gc(true)
-
     synchronize()
 end
