b13cede5fc16587b12297220e7cc8e848bc398c4
NicoMosty <npuentesu@unal.edu.co>
2022-11-26 15:20:30 -0500

    add: adding functions with struct data

--- src/run_event.jl
+++ src/run_event.jl
@@ -17,63 +17,6 @@ function cpu_simulate(X, dt, t, t_knn, args...)
     return X
 end
 
-# function simulate(PATH, SAVING, n_text, n_knn, t_f, r_max, fp, K, R_agg)
-#     # Definig Variables for calculing initial variables
-#     global X 
-#     X = X |> cu
-
-#     # Inizializate Variables for kNN
-#     global i_Cell = CuArray{Float32}(undef, (size(X, 1), size(X, 1), 3))
-#     global Dist = CuArray{Float32}(undef, (size(X, 1), size(X, 1)))
-#     global idx = hcat([[CartesianIndex(i,1) for i=1:nn] for j=1:size(X,1)]...) |> cu
-
-
-#     #Inizializate Variable for Contractile force
-#     global rand_idx = CuArray{Int32}(undef, n_knn, size(X,1))
-
-#     # Inizializate Variables for Forces
-#     global r = zeros(nn,size(X)[1],3) |> cu
-#     global r_p = zeros(size(X)) |> cu
-#     global dist = zeros(nn, size(X)[1]) |> cu
-#     global dist_p = zeros(size(X,1)) |> cu
-#     global F = zeros(nn, size(X)[1],3) |> cu
-#     global dX = zeros(size(X)[1],3) |> cu;
-
-#     # Calculating position of every cell on the fusion
-#     p = Progress(Int(t_f/dt),barlen=25)
-#     for t in 0:Int(t_f/dt)
-
-#         # Saving data in a given time (n_text)
-#         if mod(t, Int(t_f/n_text/dt)) == 0 && SAVING
-#             X_w = Matrix(X)
-#             open(PATH, "a") do f
-#                 write(f, "$(size(X, 1))\n")
-#                 write(f, "t=$(t*dt)\n")
-#                 writedlm(f,hcat(X_f, X_w), ' ')
-#             end
-#         end
-
-#         ## LEAPFROG ALGORITHM
-#         if mod(t,2) == 0
-#             if mod(t, n_knn) == 0 
-#                 # Calculating kNN
-#                 cu_knn()
-#             end
-
-#             # Finding index for random forces in a given time (size(X,1))
-#             if mod(t, 2*size(X,1)) == 0 
-#                 global rand_idx = getindex.(idx, 1)[getindex.(rand(2:nn,2*size(X,1)) ,1),:]
-#             end
-#             # Calculating Forces
-#             cu_forces(t, r_max, fp, K)
-#         else
-#             global X = X + dX*dt
-#         end
-
-#         next!(p)
-#     end
-# end
-
 function simulate(SAVING::Bool, m::Model, p::F, Agg::Aggregate) where F <: ForceType
     # Definig Variables for calculing initial variables
     global Agg
@@ -88,7 +31,6 @@ function simulate(SAVING::Bool, m::Model, p::F, Agg::Aggregate) where F <: Force
             open(m.Simulation.path, "a") do f
                 write(f, "$(size(Agg.Position.X, 1))\n")
                 write(f, "t=$(Agg.t)\n")
-                # writedlm(f,hcat(Agg.Index.Outline, Matrix(Agg.Position.X)), ' ')
                 writedlm(f,hcat(repeat([1.0],size(Agg.Position.X, 1)), X_w), ' ')
             end
         end
@@ -117,34 +59,65 @@ function simulate(SAVING::Bool, m::Model, p::F, Agg::Aggregate) where F <: Force
 
         next!(p)
     end
+
+    # Reset Aggregate with new Position of each Cell
+    Agg = Aggregate(
+        parameters.Neighbor, 
+        PositionCell(Agg.Position.X)
+    );
 end
 
-# function one_aggregate(PATH, STORE,n_text,t_f, r_max, fp, K ,R_agg, n_knn)
-    
-#     # Definig Variables for calculing the fusion
-#     global X
+function one_aggregate(SAVING::Bool, m::Model, p::F, Agg::Aggregate) where F <: ForceType
+    # Definig Variables for calculing initial variables
+    global Agg
 
-#     if R_agg isa Number
-#         # Finding External Cells on the Aggregate
-#         X_w = Matrix(X)
-#         X_f = zeros(size(X_w,1))
-#         for i in 1:size(X_f,1)
-#             A = sqrt(X_w[i,1]^2+X_w[i,2]^2+X_w[i,3]^2)
-#             if A > 0.8*R_agg
-#                 X_f[i] = 1
-#             else
-#                 X_f[i] = 2
-#             end
-#         end
-#         global X_f = Int.(X_f)
-#     else
-#         X_f = repeat([1.0], size(X,1))
-#     end
-#     # Running Forces Function
-#     println("Finding Equilibrium in one Aggregate")
-#     simulate(PATH, STORE, n_text, n_knn, t_f, r_max, fp, K, R_agg)
-#     return 
-# end
+    # Running Forces Function
+    println("Finding Equilibrium in one Aggregate")
+    simulate(SAVING, m, p, Agg)
+
+    return
+end
+
+function fusion(store, parameters, p::F, Agg) where F <: ForceType
+    # Defining Variables for calculing the fusion
+    global Agg
+
+    # Running for One Aggregate
+    one_aggregate(false, parameters, p, Agg)
+
+    # Fusioning the selected spheres
+    SumAgg(Agg::Aggregate, parameters::Model)
+
+    # Positioning tho Aggregates
+    Angle = 2*pi*rand()
+    Size = Float32(
+        (findmax(Agg.Position.X[:,1])[1] - findmin(Agg.Position.X[:,1])[1])/2 + 1
+    ) 
+
+    Move = vcat(
+        repeat(
+            [-Size 0 0],size(Agg.Position.X,1)
+        ),
+        repeat(
+            [Size 0 0],size(Agg.Position.X,1)
+        )
+    ) |> cu
+
+    Rotate = [
+        cos(Angle)   -sin(Angle)   0; 
+        sin(Angle)   cos(Angle)    0; 
+        0            0             1
+    ] |> cu
+
+    Agg.Position.X = vcat(
+        (Rotate*Agg.Position.X')', Agg.Position.X
+    ) + Move
+
+    # Running Forces Function
+    println("Finding Equilibrium in two Aggregate")
+    simulate(store, parameters, p, Agg)
+    return 
+end
 
 # function fusion(PATH,STORE, n_text,t_f, r_max, fp, K, R_agg, n_knn)
     
