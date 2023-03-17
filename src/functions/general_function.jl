931e7527575560b6075d28d64836add447af6c25
NicoMosty <npuentesu@unal.edu.co>
2023-03-16 21:40:45 -0500

    add: adding general functions

---
+++ src/functions/general_function.jl
@@ -0,0 +1,17 @@
+# Dependences
+using CUDA
+using DelimitedFiles
+
+# Euclidean Distance
+euclidean(points,i,j) = sqrt((points[i,1]-points[j,1])^2+(points[i,2]-points[j,2])^2+(points[i,3]-points[j,3])^2)
+euclidean(points,i) = sqrt((points[i,1])^2+(points[i,2])^2+(points[i,3])^2)
+
+# Step Fuctions
+function step(rmin, rmax, r)
+    return (sign(r-rmin)-sign(r-rmax))/2
+end
+
+# Find Radius of Aggregate
+function find_radius(X::Matrix)
+    return sum([abs(minimum(X[:,i])-maximum(X[:,i]))/2 for i=1:size(X,2)])/size(X,2)+0.5
+end
