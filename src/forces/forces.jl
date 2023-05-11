940d25067b460effb687febeda8944b7df74bbae
NicoMosty <npuentesu@unal.edu.co>
2023-05-10 21:26:37 -0500

    add: adding conractile force based on gabriel graph

--- src/forces/forces.jl
+++ src/forces/forces.jl
@@ -21,7 +21,7 @@ it to the total force on the point. After iterating over all neighbors, the func
 The function writes the computed force to the output array force. Finally, the position of each point is updated based on the total force acting on that 
 point and the time step.
 """
-function sum_force!(idx,points,force,force_par,dt)
+function sum_force!(idx,idx_cont,idx_sum,points,force,force_par,cont_par,dt,t_knn)
     # Defining Index for kernel
     i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
     k = (blockIdx().y - 1) * blockDim().y + threadIdx().y
@@ -29,22 +29,33 @@ function sum_force!(idx,points,force,force_par,dt)
     # Limiting data inside matrix
     if i <= size(points, 1) && k <= size(points, 2)
 
-        # Cleaning idx_sum
+        # Cleaning force
         force[i,k] = 0
 
         # Iterate on each row
-        for j=1:size(idx,1)
+        # for j=1:size(idx,1)
+        for j=1:idx_sum[i]
             # Finding forces
-            if idx[j,i] != i && idx[j,i] != 0
-                dist = euclidean(points,i,idx[j,i])
+            if idx[j,i] != 0
+                dist = euclidean(points,idx[j,i],i)
+                # if dist < force_par.rₘₐₓ[i]
                 force[i,k] += force_func(force_par,i,dist) * (points[i,k]-points[idx[j,i],k])/dist
+                # end
             end
         end
 
-        # <------------------------------------------ THIS
+        # # Adding Contractile Force
+        # random[i,k] = 2*rand() - 1
+        # force[i,k] += cont_par[i]*random[i,k]/euclidean(random,i)
+
+        # Adding Contractile Force
+        if idx_cont[t_knn,i] != i && idx_cont[t_knn,i] != 0
+            dist = euclidean(points,i,idx_cont[t_knn,i])
+            force[i,k] -= cont_par[i]*(points[i,k]-points[idx_cont[t_knn,i],k])/dist
+        end
+
         # Summing dX on the position of the aggregate on a specific dt
         points[i,k] = points[i,k] + force[i,k] * dt
-        # <------------------------------------------ THIS
         
     end
     return nothing
@@ -72,30 +83,30 @@ end
 #     return nothing
 # end
 
-"""
-# cu_force
+# """
+# # cu_force
 
-Compute the forces between each pair of particles in `agg` and their displacement.
+# Compute the forces between each pair of particles in `agg` and their displacement.
 
-    The inputs to the function are:
-        • agg   : Aggregate : The aggregate for which to compute the forces.
-        • model : ModelSet  : The model for the simulation of aggregates.
-        • t_knn : int       : The time step at which to apply the contractile force.
-"""
-function cu_force(agg::Aggregate,model::ModelSet,t_knn)
-    # GPU requirements
-    threads =(16,3)
-    blocks  =(cld.(size(agg.Position,1)+1,threads[1]),1)
+#     The inputs to the function are:
+#         • agg   : Aggregate : The aggregate for which to compute the forces.
+#         • model : ModelSet  : The model for the simulation of aggregates.
+#         • t_knn : int       : The time step at which to apply the contractile force.
+# """
+# function cu_force(agg::Aggregate,model::ModelSet,t_knn)
+#     # GPU requirements
+#     threads =(16,3)
+#     blocks  =(cld.(size(agg.Position,1)+1,threads[1]),1)
 
-    # Running GPU for calculing force
-    @cuda threads=threads blocks=blocks sum_force!(
-        agg.Simulation.Neighbor.idx_red,
-        agg.Position,
-        agg.Simulation.Force.F,
-        agg.Simulation.Parameter.Force,
-        model.Time.dt
-    )
-end
+#     # Running GPU for calculing force
+#     @cuda threads=threads blocks=blocks sum_force!(
+#         agg.Simulation.Neighbor.idx_red,
+#         agg.Position,
+#         agg.Simulation.Force.F,
+#         agg.Simulation.Parameter.Force,
+#         model.Time.dt
+#     )
+# end
 
 ################################ NEW ####################################
 # using LinearAlgebra: norm
