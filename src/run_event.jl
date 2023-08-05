e16212402310f057c413204c25d9494dc9704f34
NicoMosty <npuentesu@unal.edu.co>
2023-08-05 17:27:38 -0500

    add: new .dat format for storing

--- src/run_event.jl
+++ src/run_event.jl
@@ -1,31 +1,27 @@
+include("extract_info.jl")
+
 using ProgressMeter
 
 function run_test(agg::Aggregate, model::ModelSet, title::String,save_xyz, save_dat)
 
     open(model.Output.name_output*".xyz", "w") do f end
 
+    open(model.Output.name_output*".cart.dat", "w") do f
+        write(f, "# This file was created $(Dates.format(Dates.now(), "e, dd u yyyy HH:MM:SS")) \n")
+        write(f, "# Created by CellAggregate.jl \n")
+        write(f, "# Based on Cartesian Coordinates \n")
+        write(f, "@    xaxis label \"x position of each cell\"  \n")
+        write(f, "@    yaxis label \"y position of each cell\"  \n")
+    end
+    open(model.Output.name_output*".sph.dat", "w") do f
+        write(f, "# This file was created $(Dates.format(Dates.now(), "e, dd u yyyy HH:MM:SS")) \n")
+        write(f, "# Created by CellAggregate.jl \n")
+        write(f, "# Based on Spherical Coordinates \n")
+        write(f, "@    xaxis label \"x position of each cell\"  \n")
+        write(f, "@    yaxis label \"y position of each cell\"  \n")
+    end
+
     @showprogress "$(title)..." for t=0:Int(model.Time.tₛᵢₘ/model.Time.dt)
-        t_max_min=1
-        # println(t)
-        # CUDA.@time 
-        threads=(64,3)
-        @cuda(
-            threads = threads,
-            blocks = cld.(size(agg.Position,),threads),
-            sum_force!(
-                agg.Position,
-                agg.Simulation.Force.F,
-                agg.Simulation.Force.Pol,
-                agg.Simulation.Force.N_i,
-                agg.Simulation.Neighbor.idx_sum,
-                agg.Simulation.Neighbor.idx_red,
-                agg.Simulation.Parameter.Force,
-                agg.Simulation.Parameter.Contractile.fₚ,
-                atan(1),
-                pi/4,
-                model.Time.dt
-            )
-        )
 
         if t%(model.Time.nₖₙₙ) == 0
             # println("▲ Neighbor")
@@ -43,46 +39,60 @@ function run_test(agg::Aggregate, model::ModelSet, title::String,save_xyz, save_
                 )
             ) 
         end
+
+        t_max_min=1
+        # println(t)
+        # CUDA.@time 
+        threads=(64,3)
+        @cuda(
+            threads = threads,
+            blocks = cld.(size(agg.Position,),threads),
+            sum_force!(
+                agg.Position,
+                agg.Simulation.Force.F,
+                agg.Simulation.Force.Pol,
+                agg.Simulation.Force.dPol,
+                agg.Simulation.Force.N_i,
+                agg.Simulation.Neighbor.idx_sum,
+                agg.Simulation.Neighbor.idx_red,
+                agg.Simulation.Parameter.Force,
+                agg.Simulation.Parameter.Contractile.fₚ,
+                agg.Simulation.Parameter.Contractile.ψₜ,
+                agg.Simulation.Parameter.Contractile.ψₘ,
+                agg.Simulation.Parameter.Contractile.ω,
+                model.Time.dt
+            )
+        )
  
-        if t%Int(model.Time.tₛᵢₘ/model.Time.nₛₐᵥₑ/model.Time.dt) == 0 && save_xyz
+        if t%Int(model.Time.tₛᵢₘ/model.Time.nₛₐᵥₑ/model.Time.dt) == 0
             # println("▲ Save")
-            open(model.Output.name_output*".xyz", "a") do f
-                write(f, "$(size(agg.Position, 1))\n")
-                write(f, "t=$(t)\n")
-                writedlm(f,hcat(agg.Geometry.outline,Matrix(agg.Position)), ' ')
+            push!(agg.Simulation.Output.time,t*model.Time.dt)
+
+            max_min = max_min_agg(agg.Position,40,10,2)
+            push!(agg.Simulation.Output.xy_data,max_min)
+
+            sph_data = cart_to_sph(max_min)
+            push!(agg.Simulation.Output.θr_data,sph_data)
+
+            if save_xyz
+                open(model.Output.name_output*".xyz", "a") do f
+                    write(f, "$(size(agg.Position, 1))\n")
+                    write(f, "t=$(t*model.Time.dt)\n")
+                    writedlm(f,hcat(agg.Geometry.outline,Matrix(agg.Position)), ' ')
+                end
             end
             if save_dat
-                agg.Simulation.Output.outline_data[floor(Int32,t/model.Time.tₛᵢₘ*model.Time.nₛₐᵥₑ*model.Time.dt+1),:] = vcat(
-                    [
-                        [
-                            extrema(
-                                Matrix(agg.Position)[:,2][-q .< Matrix(agg.Position)[:,1] .<= -q+1],
-                                init=(0.0,0.0)
-                        )...] 
-                        for q in max_min_agg(agg.Geometry.range_x,model.Output.d_saved)
-                    ]
-                ...)
-            end
-        end
-    end
+                open(model.Output.name_output*".cart.dat", "a") do f
+                    write(f, "!    t = $(t*model.Time.dt)  \n")
+                    writedlm(f,max_min')
+                end
 
-    if save_dat
-        open(model.Output.name_output*".dat", "w") do f
-            write(f, "# This file was created $(Dates.format(Dates.now(), "e, dd u yyyy HH:MM:SS")) \n")
-            write(f, "# Created by CellAggregate.jl \n")
-            write(f, "@    title \"Outline Cells on The Aggregate Evolution\"  \n")
-            write(f, "@    xaxis label \"y position of each cell\"  \n")
-            write(f, "@    yaxis label \"Snaps Taken (1/t_sim)\"  \n")
-            write(f, "@TYPE xy  \n")
-            write(f, "@ view 0.15, 0.15, 0.75, 0.85  \n")
-            write(f, "@ legend on  \n")
-            write(f, "@ legend box on \n")
-            write(f, "@ legend loctype view \n")
-            write(f, "@ legend 0.78, 0.8 \n")
-            write(f, "@ legend length 2 \n")
-            write(f, "@ legend 0.78, 0.8 \n")
-            write(f, "@ legend length 2 \n")
-            writedlm(f,hcat(agg.Simulation.Output.x_axis,agg.Simulation.Output.outline_data'), ' ')
+                open(model.Output.name_output*".sph.dat", "a") do f
+                    write(f, "!    t = $(t*model.Time.dt)  \n")
+                    writedlm(f,sph_data')
+                end
+
+            end
         end
     end
 
