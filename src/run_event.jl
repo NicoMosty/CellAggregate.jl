5891531251bd0d9e28eccdb478babd23cbcc6685
NicoMosty <npuentesu@unal.edu.co>
2023-09-03 16:58:10 -0500

    delete: deleting deprecated  saved files

--- src/run_event.jl
+++ src/run_event.jl
@@ -2,26 +2,26 @@
 
 # using ProgressMeter
 
-function run_test(agg::Aggregate, model::ModelSet, title::String,save_xyz, save_dat)
+function run_test(agg::Aggregate, model::ModelSet, title::String,save_xyz, save_dat, xyz_type)
     if save_xyz
-        open(model.Output.name_output*model.Output.name_output*".xyz", "w") do f end
+        open(model.Output.path_output*model.Output.name_output*".xyz", "w") do f end
     end
 
     if save_dat
-        open(model.Output.path_output*model.Output.name_output*".cart.dat", "w") do f
-            write(f, "# This file was created $(Dates.format(Dates.now(), "e, dd u yyyy HH:MM:SS")) \n")
-            write(f, "# Created by CellAggregate.jl \n")
-            write(f, "# Based on Cartesian Coordinates \n")
-            write(f, "@    xaxis label \"x position of each cell\"  \n")
-            write(f, "@    yaxis label \"y position of each cell\"  \n")
-        end
-        open(model.Output.path_output*model.Output.name_output*".sph.dat", "w") do f
-            write(f, "# This file was created $(Dates.format(Dates.now(), "e, dd u yyyy HH:MM:SS")) \n")
-            write(f, "# Created by CellAggregate.jl \n")
-            write(f, "# Based on Spherical Coordinates \n")
-            write(f, "@    xaxis label \"x position of each cell\"  \n")
-            write(f, "@    yaxis label \"y position of each cell\"  \n")
-        end
+        # open(model.Output.path_output*model.Output.name_output*".cart.dat", "w") do f
+        #     write(f, "# This file was created $(Dates.format(Dates.now(), "e, dd u yyyy HH:MM:SS")) \n")
+        #     write(f, "# Created by CellAggregate.jl \n")
+        #     write(f, "# Based on Cartesian Coordinates \n")
+        #     write(f, "@    xaxis label \"x position of each cell\"  \n")
+        #     write(f, "@    yaxis label \"y position of each cell\"  \n")
+        # end
+        # open(model.Output.path_output*model.Output.name_output*".sph.dat", "w") do f
+        #     write(f, "# This file was created $(Dates.format(Dates.now(), "e, dd u yyyy HH:MM:SS")) \n")
+        #     write(f, "# Created by CellAggregate.jl \n")
+        #     write(f, "# Based on Spherical Coordinates \n")
+        #     write(f, "@    xaxis label \"x position of each cell\"  \n")
+        #     write(f, "@    yaxis label \"y position of each cell\"  \n")
+        # end
         open(model.Output.path_output*model.Output.name_output*".nw.dat", "w") do f
             write(f, "# This file was created $(Dates.format(Dates.now(), "e, dd u yyyy HH:MM:SS")) \n")
             write(f, "# Created by CellAggregate.jl \n")
@@ -32,16 +32,15 @@ function run_test(agg::Aggregate, model::ModelSet, title::String,save_xyz, save_
 
     @showprogress "$(title)..." for t=0:Int(model.Time.tₛᵢₘ/model.Time.dt)
 
-        if agg.Simulation.Limit.break_sim == CuArray([false])
+        # if agg.Simulation.Limit.break_sim == CuArray([false])
             if t%(model.Time.nₖₙₙ) == 0
                 # println("▲ Neighbor")
-                threads=(100)
+                threads_n=(100)
                 @cuda(
-                    threads=threads,
-                    blocks=cld.(size(agg.Position,1),threads),
+                    threads=threads_n,
+                    blocks=cld.(size(agg.Position,1),threads_n),
                     dist_kernel!(
                         agg.Simulation.Neighbor.idx_red,
-                        agg.Simulation.Neighbor.idx_cont,
                         agg.Simulation.Neighbor.idx_sum,
                         agg.Simulation.Neighbor.dist,
                         agg.Position,
@@ -53,10 +52,10 @@ function run_test(agg::Aggregate, model::ModelSet, title::String,save_xyz, save_
             t_max_min=1
             # println(t)
             # CUDA.@time 
-            threads=(64,3)
+            threads_f=(64,3)
             @cuda(
-                threads = threads,
-                blocks = cld.(size(agg.Position,),threads),
+                threads = threads_f,
+                blocks = cld.(size(agg.Position,),threads_f),
                 sum_force!(
                     agg.Position,
                     agg.Simulation.Force.F,
@@ -77,58 +76,73 @@ function run_test(agg::Aggregate, model::ModelSet, title::String,save_xyz, save_
             )
     
             if t%Int(model.Time.tₛᵢₘ/model.Time.nₛₐᵥₑ/model.Time.dt) == 0
-                # println("▲ Save")
-                push!(agg.Simulation.Output.time,t*model.Time.dt)
+                # # println("▲ Save")
+                # push!(agg.Simulation.Output.time,t*model.Time.dt)
 
-                max_min = max_min_agg(agg.Position,40,10,2)
-                push!(agg.Simulation.Output.xy_data,max_min)
+                # max_min = max_min_agg(agg.Position,40,10,2)
+                # push!(agg.Simulation.Output.xy_data,max_min)
 
-                sph_data = cart_to_sph(max_min)
-                push!(agg.Simulation.Output.θr_data,sph_data)
+                # sph_data = cart_to_sph(max_min)
+                # push!(agg.Simulation.Output.θr_data,sph_data)
 
-                neck_width = neck_width_agg(sph_data)
-                push!(agg.Simulation.Output.neck_data,[neck_width[2]])
-                push!(agg.Simulation.Output.width_data,[neck_width[1]])
+                # neck_width = neck_width_agg(sph_data)
+                # push!(agg.Simulation.Output.neck_data,[neck_width[2]])
+                # push!(agg.Simulation.Output.width_data,[neck_width[1]])
 
                 if save_xyz
                     open(model.Output.path_output*model.Output.name_output*".xyz", "a") do f
                         write(f, "$(size(agg.Position, 1))\n")
                         write(f, "t=$(t*model.Time.dt)\n")
-                        writedlm(f,hcat(agg.Geometry.outline,Matrix(agg.Position)), ' ')
+                        writedlm(f,hcat(xyz_type,Matrix(agg.Position)), ' ')
                     end
                 end
                 if save_dat
-                    open(model.Output.path_output*model.Output.name_output*".cart.dat", "a") do f
-                        write(f, "!    t = $(t*model.Time.dt)  \n")
-                        writedlm(f,max_min')
+
+                    data_cell = Matrix(agg.Position)
+
+                    polar_idx = hcat(
+                        [data_cell[i,2] >= 1 ? pi/2-atan(data_cell[i,1]/data_cell[i,2]) : 3*pi/2-atan(data_cell[i,1]/data_cell[i,2]) for i=1:size(data_cell,1)] ,
+                        sqrt.(sum(data_cell .^ 2, dims=2))
+                    )
+            
+                    lin_idx = hcat(cos.(2 .*polar_idx[:,1]),polar_idx[:,2])
+                    
+                    MAX = maximum(lin_idx[lin_idx[:,1] .> 0.95,2])
+                    MIN_list = lin_idx[lin_idx[:,1] .< -0.95,2]
+                    
+                    MIN = 0
+                    if size(MIN_list,1) > 0
+                        global MIN = maximum(MIN_list)
+                    end
+    
+                    if size(agg.Simulation.Output.time, 1) == 0
+                        global NMAX = maximum(lin_idx[lin_idx[:,1] .> 0.95,2])
+                    end
+    
+                    MAX = MAX/NMAX; MIN = MIN/NMAX;
+    
+                    push!(agg.Simulation.Output.time, t)
+                    push!(agg.Simulation.Output.neck_data, MIN)
+                    push!(agg.Simulation.Output.width_data, MAX)
+
+                    open(model.Output.path_output*model.Output.name_output*".nw.dat", "a") do f
+                        writedlm(f,[t*model.Time.dt, MIN, MAX]', ' ')
                     end
 
-                    open(model.Output.path_output*model.Output.name_output*".sph.dat", "a") do f
-                        write(f, "!    t = $(t*model.Time.dt)  \n")
-                        writedlm(f,sph_data')
-                    end
+                    # open(model.Output.path_output*model.Output.name_output*".cart.dat", "a") do f
+                    #     write(f, "!    t = $(t*model.Time.dt)  \n")
+                    #     writedlm(f,max_min')
+                    # end
+
+                    # open(model.Output.path_output*model.Output.name_output*".sph.dat", "a") do f
+                    #     write(f, "!    t = $(t*model.Time.dt)  \n")
+                    #     writedlm(f,sph_data')
+                    # end
 
                 end
             end
-        end
+        # end
 
-    end
-
-    if agg.Simulation.Limit.break_sim == CuArray([false])
-        agg.Simulation.Output.neck_data  = hcat(agg.Simulation.Output.neck_data...) ./agg.Simulation.Output.width_data[1] .* 2
-        agg.Simulation.Output.width_data = hcat(agg.Simulation.Output.width_data...) ./agg.Simulation.Output.width_data[1] .* 2
-        if save_dat
-            open(model.Output.path_output*model.Output.name_output*".nw.dat", "a") do f
-                writedlm(f,vcat(
-                        hcat(agg.Simulation.Output.time...),
-                        hcat(agg.Simulation.Output.neck_data...),
-                        hcat(agg.Simulation.Output.width_data...)
-                    )' ./agg.Simulation.Output.width_data[1]
-                )
-            end
-        end
-    elseif agg.Simulation.Limit.break_sim == CuArray([true])
-        println("Breaking the Simulation (NaN Value or Bigger Values)")
     end
 
 end
