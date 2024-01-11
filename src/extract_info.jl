4fb99e5dd46ab2619a51309633afe7dae7714f18
NicoMosty <npuentesu@unal.edu.co>
2024-01-11 13:08:08 -0500

    delete: deprecated functions

--- src/extract_info.jl
+++ src/extract_info.jl
@@ -88,7 +88,6 @@ end
 # review this
 
 linearized_func(data,N) = abs(cos(data))^N
-mean_val(x) = sum(x)/size(x,1)
 
 function compare_filenames(a::AbstractString, b::AbstractString)
     # Extract numeric parts using regular expressions
@@ -100,8 +99,8 @@ end
 
 function min_max_val(data)
     return(
-        mean_val(data[:,2][data[:,1] .< 0.05]),
-        mean_val(data[:,2][data[:,1] .> 0.95])
+        mean_val(data[:,2][data[:,1] .< 0.05])[1],
+        mean_val(data[:,2][data[:,1] .> 0.95])[1]
     )
 end
 
@@ -122,157 +121,3 @@ function lin_eq(data)
 end
 
 R_2(data, predicted) = 1 - sum((data[:,2] - predicted[:,2]) .^2)/sum((data[:,2] .-sum(data[:,2])/size(data[:,2],1)) .^2)
-
-# REVIEW THIS
-# #=
-# -----------------------------------------------------------------------------
-# ----------------     FUNCTIONS FOR AGGREGATES IN LITERATURE      ------------
-# -----------------------------------------------------------------------------
-# =#
-# function grayscale(name::String, scale)
-#     #Loading Image
-#     img = load(name)
-
-#     # Filtering image in grey scale
-#     Gray_img = .! (Gray.(img) .> scale)
-#     return Gray_img
-# end
-
-# function contour(Gray_img, reduce)
-#     # Finding the contour from image
-#     Gray_img_contour = abs.(Gray_img - circshift(Gray_img,2))
-#     Gray_img_contour = Gray_img_contour[reduce:size(Gray_img_contour)[1]-reduce,reduce:size(Gray_img_contour)[2]-reduce]
-    
-#     # Finding the coordinates from contour from image
-#     idx = CartesianIndices(size(Gray_img_contour))[Gray_img_contour .== 1]
-#     arr_idx = hcat(getindex.(idx,2),getindex.(idx,1))
-
-#     return arr_idx
-# end
-
-# function center_mass(Gray_img)
-#     # Calculating the Center of Mass of Image
-#     sum_x = sum(Gray_img, dims=1) .* collect(1:size(Gray_img,2))'
-#     sum_y = sum(Gray_img, dims=2) .* collect(1:size(Gray_img,1))
-#     center_of_mass = hcat(sum(sum_x),sum(sum_y)) ./ sum(Gray_img)
-
-#     return center_of_mass
-# end
-
-# function cil_coord(Gray_img, reduce)
-#     # Putting the index in the center of mass
-#     center_idx = contour(Gray_img, reduce) - repeat(center_mass(Gray_img), size(contour(Gray_img, reduce), 1))
-
-#     r = sqrt.(sum(center_idx .^ 2, dims=2))
-#     θ = atan.(center_idx[:,2] ./ center_idx[:,1]) + pi*[center_idx[:,2] .< 0][1]
-#     center_idx_cil_coord = hcat(θ,r)
-#     return center_idx_cil_coord
-# end
-
-# function compare_plt(dir, red, scale)
-#     X = []
-#     Y = []
-
-#     for i in dir
-#         push!(X, 180/pi .* cil_coord(grayscale(i, scale), red)[:,1] .+ 90)
-#         push!(Y, cil_coord(grayscale(i, scale), red)[:,2])
-#         # println("-----------------------------------------")
-#     end
-
-#     scatter(X, Y, 
-#             labels=dir, 
-#             shape=[:+ :o :utri], 
-#             markersize=1,
-#             xticks = 0:45:360
-#     )
-# end
-
-# function compare_plt_norm(dir, red, scale)
-#     X = []
-#     Y = []
-
-#     for i in dir
-#         push!(X, 180/pi .* cil_coord(grayscale(i, scale), red)[:,1] .+ 90)
-#         push!(Y, cil_coord(grayscale(i,scale), red)[:,2])
-#     end
-
-#     Y = 2 .* Y ./ findmax(vcat(Y...))[1]
-#     scatter(X, Y, 
-#             labels=dir, 
-#             shape=[:+ :o :utri], 
-#             markersize=1,
-#             xticks = 0:45:360
-#     )
-# end
-
-# #=
-# -----------------------------------------------------------------------------
-# ------------------     FUNCTIONS FOR CALCULATED AGGREGATES      -------------
-# -----------------------------------------------------------------------------
-# =#
-# function corr_data(X_f)
-#     # Finding the center of mass
-#     X_center = sum(X_f, dims=1)./ size(X_f[:,1])
-
-#     # # Moving two aggregates to the center of mass
-#     X_f = X_f - repeat(X_center ,size(X_f)[1])
-
-#     # Cilindrical Coordinates
-#     X_f_cil = zeros(size(X_f)[1],3)
-#     X_f_cil[:,1] = sqrt.(sum(X_f .^ 2, dims=2))
-#     X_f_cil[:,2:3]  = mod.(180/pi .* (atan.(X_f[:,2:3] ./ X_f[:,1]) + pi*[X_f[:,2:3] .< 0][1]) .+ 90, 360)
-
-#     # Correction of angle with the max radius on the agregate
-#     correct = X_f_cil[findmax(X_f_cil[:,1])[2],:][2:3] .* (pi/180)
-#     correct = map(x -> x > 180 ? x - 180 : x, correct)
-#     correct = [-pi/2 -pi/2]' + correct
-
-#     # Using the matrix for rotate the aggregeate
-#     Mat_y = [cos(correct[1]) 0 sin(correct[1]); 0 1 0;
-#             -sin(correct[1]) 0 cos(correct[1]) ]
-
-#     Mat_z = [cos(correct[2]) -sin(correct[2]) 0; 
-#             sin(correct[2]) cos(correct[2]) 0; 0 0 1 ]
-
-#     return (Mat_y * Mat_z * X_f')'
-# end
-# function compare(k)
-#     # Calling Data from files
-#     id = Float64.(readdlm("T_150000/rmax_3.5_s_1.9/k_$(k)/Test_Initial.xyz")[3:end,1])
-#     X_i = Float64.(readdlm("T_150000/rmax_3.5_s_1.9/k_$(k)/Test_Initial.xyz")[3:end,2:4])
-#     X_f = Float64.(readdlm("T_150000/rmax_3.5_s_1.9/k_$(k)/Test_Final.xyz")[3:end,2:4])
-#     X_f = corr_data(X_f)
-    
-#     X_i = hcat(X_i[:,1],X_i[:,3])
-#     X_f= hcat(X_f[:,1],X_f[:,3]);
-
-#     # Finding the center of mass
-#     X_i_center = sum(X_i, dims=1) ./ size(X_i)[1]
-#     X_f_center = sum(X_f, dims=1) ./ size(X_f)[1]
-
-#     # Moving two aggregates to the center of mass
-#     X_i = X_i - repeat(X_i_center, size(X_i)[1])
-#     X_f = X_f - repeat(X_f_center, size(X_f)[1])
-
-#     # Cilindrical Coordinates
-#     r_i = sqrt.(sum(X_i .^ 2, dims=2))
-#     θ_i = mod.(180/pi .* (atan.(X_i[:,2] ./ X_i[:,1]) + pi*[X_i[:,2] .< 0][1]) .+ 90, 360)
-#     X_i_cil = hcat(θ_i,r_i)
-
-#     r_f = sqrt.(sum(X_f .^ 2, dims=2))
-#     θ_f = mod.(180/pi .* (atan.(X_f[:,2] ./ X_f[:,1]) + pi*[X_f[:,2] .< 0][1]) .+ 90, 360)
-#     X_f_cil = hcat(θ_f,r_f)
-
-#     return X_i_cil, X_f_cil
-# end
-
-# function plot_compare(X_i, X_f)
-#     scatter(
-#         [X_i[:,1], X_f[:,1]], 
-#         [X_i[:,2], X_f[:,2]], 
-#         labels=["Init" "Final"], 
-#         shape=[:+ :o :utri], 
-#         markersize=3,
-#         xticks = 0:45:360
-#     )
-# end
